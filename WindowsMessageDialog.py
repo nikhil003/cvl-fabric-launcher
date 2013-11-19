3b7b5cc9f91ff5383da3ccc2a10a10e216e4da6c
Chris Hines <chris.hines@monash.edu>
2013-11-19 16:10:59 +1100

    remove almost all magic numbers from the Linux and Windows MessageDialog modules

--- WindowsMessageDialog.py
+++ WindowsMessageDialog.py
@@ -5,19 +5,22 @@ import IconPys.MASSIVElogoTransparent64x64
 class LauncherMessageDialog(wx.Dialog):
     def __init__(self, parent, message, title, helpEmailAddress="help@massive.org.au",ButtonLabels=['OK'],**kw):
         wx.Dialog.__init__(self, parent, style=wx.DEFAULT_DIALOG_STYLE|wx.STAY_ON_TOP, **kw)
+
+        self.helpEmailAddress=helpEmailAddress
+        self.ButtonLabels=ButtonLabels
        
         if parent!=None:
             self.CenterOnParent()
         else:
             self.Centre()
-
-        self.helpEmailAddress=helpEmailAddress
-        self.dialogPanel = wx.Panel(self, wx.ID_ANY)
         self.SetSizer(wx.BoxSizer(wx.VERTICAL))
-        self.ButtonLabels=ButtonLabels
+        topPanel=wx.Panel(self,wx.ID_ANY)
+        topPanel.SetSizer(wx.BoxSizer(wx.HORIZONTAL))
+
 
         iconAsBitmap = IconPys.MASSIVElogoTransparent64x64.getMASSIVElogoTransparent64x64Bitmap()
-        self.iconBitmap = wx.StaticBitmap(self.dialogPanel, wx.ID_ANY, iconAsBitmap, pos=(25,15), size=(64,64))
+        icon = wx.StaticBitmap(topPanel, wx.ID_ANY, iconAsBitmap, size=(64,64))
+        topPanel.GetSizer().Add(icon,flag=wx.ALL,border=5)
 
 
         #self.setTitle(title)
@@ -26,48 +29,45 @@ class LauncherMessageDialog(wx.Dialog):
 #        smallFont.SetPointSize(11)
 
         messageWidth = 330
-        self.messageLabel = wx.StaticText(self.dialogPanel, wx.ID_ANY, message, pos=(105,39), size=(messageWidth,-1))
+        self.messageLabel = wx.StaticText(topPanel, wx.ID_ANY, message)
+        self.messageLabel.SetMinSize((messageWidth,-1))
         #self.messageLabel = wx.StaticText(self.dialogPanel, wx.ID_ANY, message)
-        self.messageLabel.SetForegroundColour((0,0,0))
+        #self.messageLabel.SetForegroundColour((0,0,0))
 #        self.messageLabel.SetFont(smallFont)
-        self.messageLabel.Wrap(messageWidth)
+#        self.messageLabel.Wrap(messageWidth)
+        topPanel.GetSizer().Add(self.messageLabel,flag=wx.EXPAND|wx.ALL,proportion=1,border=5)
 
-        buttonSize = wx.Size(72,22)
-        buttonPanel = wx.Panel(self.dialogPanel,wx.ID_ANY)
+        #buttonSize = wx.Size(72,22)
+        bottomPanel=wx.Panel(self,wx.ID_ANY)
+        bottomPanel.SetSizer(wx.BoxSizer(wx.HORIZONTAL))
+        buttonPanel = wx.Panel(bottomPanel,wx.ID_ANY)
         buttonPanel.SetSizer(wx.BoxSizer(wx.HORIZONTAL))
         for label in ButtonLabels:
             #b = wx.Button(buttonPanel, wx.ID_ANY, label,size=buttonSize)
             b = wx.Button(buttonPanel, wx.ID_ANY, label)
             b.SetDefault()
             b.Bind(wx.EVT_BUTTON,self.onClose)
-            buttonPanel.GetSizer().Add(b,border=5)
+            buttonPanel.GetSizer().Add(b,flag=wx.ALL,border=5)
         buttonPanel.Fit()
 
 
-        dialogPanelWidth = max(messageWidth,buttonPanel.GetSize().width)+145
-        dialogPanelHeight = max(self.messageLabel.GetPosition().y + self.messageLabel.GetSize().height + 70, 140)
-
-        self.SetClientSize(wx.Size(dialogPanelWidth,dialogPanelHeight))
-        self.dialogPanel.SetSize(wx.Size(dialogPanelWidth,dialogPanelHeight))
-
-        #self.GetSizer().Add(buttonPanel)
-        
-        buttonPosition = wx.Point(dialogPanelWidth - buttonPanel.GetSize().width - 25 , dialogPanelHeight - buttonPanel.GetSize().height - 18)
-        buttonPanel.SetPosition(buttonPosition)
-
         self.Bind(wx.EVT_CLOSE, self.onClose)
-        #self.Bind(wx.EVT_BUTTON, self.onClose, id=self.okButton.GetId())
 
-        self.contactQueriesContactLabel = wx.StaticText(self.dialogPanel, label = "For queries, please contact:")
+        self.contactQueriesContactLabel = wx.StaticText(bottomPanel, label = "For queries, please contact:")
 #        self.contactQueriesContactLabel.SetFont(smallFont)
-        self.contactQueriesContactLabel.SetForegroundColour(wx.Colour(0,0,0))
-        self.contactQueriesContactLabel.SetPosition(wx.Point(25,buttonPosition.y))
+#        self.contactQueriesContactLabel.SetForegroundColour(wx.Colour(0,0,0))
+#        self.contactQueriesContactLabel.SetPosition(wx.Point(25,buttonPosition.y))
+        bottomPanel.GetSizer().Add(self.contactQueriesContactLabel,flag=wx.ALIGN_CENTER|wx.ALL,border=5)
 
-        self.contactEmailHyperlink = wx.HyperlinkCtrl(self.dialogPanel, id = wx.ID_ANY, label = self.helpEmailAddress, url = "mailto:" + self.helpEmailAddress)
+        self.contactEmailHyperlink = wx.HyperlinkCtrl(bottomPanel, id = wx.ID_ANY, label = self.helpEmailAddress, url = "mailto:" + self.helpEmailAddress)
 #        self.contactEmailHyperlink.SetFont(smallFont) # Or maybe even smaller font?
         #hyperlinkPosition = wx.Point(self.contactQueriesContactLabel.GetPosition().x+self.contactQueriesContactLabel.GetSize().width+10,okButtonPosition.y)
-        hyperlinkPosition = wx.Point(self.contactQueriesContactLabel.GetPosition().x+self.contactQueriesContactLabel.GetSize().width,buttonPosition.y)
-        self.contactEmailHyperlink.SetPosition(hyperlinkPosition)
+#        hyperlinkPosition = wx.Point(self.contactQueriesContactLabel.GetPosition().x+self.contactQueriesContactLabel.GetSize().width,buttonPosition.y)
+        bottomPanel.GetSizer().Add(self.contactEmailHyperlink,flag=wx.ALIGN_CENTER|wx.ALL,border=5)
+        bottomPanel.GetSizer().Add(buttonPanel,flag=wx.ALIGN_CENTER|wx.LEFT,border=50)
+        self.GetSizer().Add(topPanel,flag=wx.EXPAND,proportion=1)
+        self.GetSizer().Add(bottomPanel)
+        self.Fit()
 
     def onClose(self, event):
         obj=event.GetEventObject()
