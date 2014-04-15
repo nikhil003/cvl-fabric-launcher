f5f336f2078341d0358d73924f7f4484e82368a0
Chris Hines <chris.hines@monash.edu>
2014-04-16 07:17:23 +1000

    attempt to fix looking for the masterList.url in linux and mac builds. I'm not sure what the correct path is for py2exe builds

--- launcher.py
+++ launcher.py
@@ -680,7 +680,10 @@ class LauncherMainFrame(wx.Frame):
     def getNewSites(self,queue):
         newlist=[]
         try:
-            f=open(os.path.join(sys.path[0],"masterList.url"),'r')
+            if sys.platform.startswith("linux"):
+                f=open(os.path.join(sys.path[0],"masterList.url"),'r')
+            else:
+                f=open("masterList.url",'r')
             url=f.read().rstrip()
             logger.debug("master list of sites is available at %s"%url)
             newlist=siteConfig.getMasterSites(url)
