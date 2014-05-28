a13fb6fea0dcaa15d2287ca5a4fa1d087432a9cf
Chris Hines <chris.hines@monash.edu>
2014-05-28 14:36:05 +1000

    updates to packing ubuntu version so I can use fakeroot

--- package_ubuntu_version.sh
+++ package_ubuntu_version.sh
@@ -3,10 +3,11 @@
 VERSION=`grep '^version_number' launcher_version_number.py | cut -f 2 -d '"'`
 ARCHITECTURE=`uname -m | sed s/x86_64/amd64/g | sed s/i686/i386/g`
 
-./package_linux_version.sh $VERSION $ARCHITECTURE
+#./package_linux_version.sh $VERSION $ARCHITECTURE
 
 TMP="tmp_debian_build"
 
+alias sudo=""
 sudo rm -fr $TMP
 sudo rm -f *.deb
 
