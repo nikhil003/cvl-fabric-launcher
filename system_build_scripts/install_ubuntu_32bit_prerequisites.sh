6e458a54f7fdc35eca759f0bbccd9f7b14e3704d
Chris Hines <chris.hines@monash.edu>
2014-07-04 11:18:48 +1000

    update the location for the get-pip.py script used in installing dependencies for linux builds

--- system_build_scripts/install_ubuntu_32bit_prerequisites.sh
+++ system_build_scripts/install_ubuntu_32bit_prerequisites.sh
@@ -4,7 +4,7 @@ set -x
 set -e
 
 curl http://python-distribute.org/distribute_setup.py | python
-curl https://raw.github.com/pypa/pip/master/contrib/get-pip.py | python
+curl https://raw.githubusercontent.com/pypa/pip/master/contrib/get-pip.py | python
 
 pip install ssh
 pip install pycrypto
