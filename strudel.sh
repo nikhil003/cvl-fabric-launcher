d69331c7e7232cbba686f88c6dd0b7c04cbfd3d8
Jens Henrik Goebbert <j.goebbert@fz-juelich.de>
2016-06-02 16:03:31 +0200

    back to master/origin

--- strudel.sh
+++ strudel.sh
@@ -2,14 +2,6 @@
 
 STRUDEL_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# add included libgio if not avail on system - use system if avail
-LIBGIO_PATH=$(/sbin/ldconfig -p | grep  libgio-2.0.so.0)
-if [ -z "$LIBGIO_PATH" ]; then
-  echo "WARNING: libgio-2.0.so.0 not found on system - using build in version"
-  LD_LIBRARY_PATH="${STRUDEL_DIR}"/bin/libgio:$LD_LIBRARY_PATH
-fi
-
-# start launcher
 if [ -f launcher ]; then
   LD_LIBRARY_PATH="${STRUDEL_DIR}":$LD_LIBRARY_PATH
   "${STRUDEL_DIR}"/launcher
